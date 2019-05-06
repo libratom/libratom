7c5769637c83365faf5898905e3fc28d9bbef65c
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-05-06 13:13:11 -0400

    Add PffArchive class (#3)

--- libratom/utils/pff.py
+++ libratom/utils/pff.py
@@ -1,11 +1,47 @@
 import logging
+from collections import deque
+from io import IOBase
+from pathlib import Path
+import pypff
 
 logger = logging.getLogger(__name__)
 
-try:
-    import pypff
-except ImportError:
-    logger.exception(
-        f"No pypff support found. {__name__} requires libpff and its python bindings"
-    )
-    raise
+
+class PffArchive:
+    def __init__(self, file=None):
+        self.data = pypff.file()
+
+        if file:
+            self.load(file)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self.data.close()
+
+    def load(self, file):
+        if isinstance(file, IOBase):
+            self.data.open_file_object(file)
+        elif isinstance(file, Path) or isinstance(file, str):
+            self.data.open(str(file), 'rb')
+        else:
+            raise TypeError(f'Unable to load {file} of type {type(file)}')
+
+    def folders(self, bfs=True):
+        folders = deque([self.data.root_folder])
+
+        while folders:
+            folder = folders.pop()
+
+            yield folder
+
+            if bfs:
+                folders.extendleft(folder.sub_folders)
+            else:
+                folders.extend(folder.sub_folders)
+
+    def messages(self, bfs=True):
+        for folder in self.folders(bfs):
+            for message in folder.sub_messages:
+                yield message
--- tests/test_libratom.py
+++ tests/test_libratom.py
@@ -1,11 +1,17 @@
 # pylint: disable=missing-docstring
 import libratom
+from libratom.utils.pff import PffArchive
 
 
 def test_version():
     assert libratom.__version__
 
 
-def test_extract_message(enron_dataset_part003):
-    pass
+def test_extract_plain_text_messages(enron_dataset_part003):
 
+    # Get the first PST file
+    pst_file = next(enron_dataset_part003.glob('*.pst'))
+
+    with PffArchive(pst_file) as archive:
+        for message in archive.messages():
+            assert message.plain_text_body
