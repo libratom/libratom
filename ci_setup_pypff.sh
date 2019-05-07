ec66a193591b5e9ee9801009fe723c285e81ecf7
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-05-07 14:03:22 -0400

    Improve CI setup script (#3)

--- ci_setup_pypff.sh
+++ ci_setup_pypff.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-[[ $CONTINUOUS_INTEGRATION ]] || exit 0
+export TEST_DIR=${LIBRATOM_TEST_DIR:="/tmp/libratom"}
+export LIBPFF_ROOT_DIR="${TEST_DIR}/libpff-20180714"
 
-# Download libpff and configure it with python support
-export TEST_DIR="/tmp/libratom"
-mkdir -p $TEST_DIR
-cd $TEST_DIR
-wget https://github.com/libyal/libpff/releases/download/20180714/libpff-experimental-20180714.tar.gz
-tar -xzvf libpff-experimental-20180714.tar.gz
-cd libpff-20180714/
-./configure --enable-python3
+if [[ $CONTINUOUS_INTEGRATION ]]; then
 
-# Build and install the python bindings
-python setup.py build
+    # Download libpff
+    mkdir -p $TEST_DIR
+    cd $TEST_DIR
+    wget https://github.com/libyal/libpff/releases/download/20180714/libpff-experimental-20180714.tar.gz
+    tar -xzvf libpff-experimental-20180714.tar.gz
+
+    # Configure libpff with python support
+    cd $LIBPFF_ROOT_DIR
+    ./configure --enable-python3
+
+    # Build pypff
+    python setup.py build
+
+fi
+
+# Install pypff
+cd $LIBPFF_ROOT_DIR
 python setup.py install
