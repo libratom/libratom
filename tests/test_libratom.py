757a32146f915f3c139ccd679e9d00be126206f7
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-05-07 14:59:37 -0400

    Expand unit testing (#3)

--- tests/test_libratom.py
+++ tests/test_libratom.py
@@ -1,4 +1,6 @@
 # pylint: disable=missing-docstring
+import pytest
+
 import libratom
 from libratom.utils.pff import PffArchive
 
@@ -7,11 +9,27 @@ def test_version():
     assert libratom.__version__
 
 
-def test_extract_plain_text_messages(enron_dataset_part003):
+def test_pffarchive_load_from_file_object(sample_pst_file):
 
-    # Get the first PST file of this enron subset
-    pst_file = next(enron_dataset_part003.glob('*.pst'))
+    with sample_pst_file.open(mode='rb') as f, PffArchive(f) as archive:
+        assert len([message for message in archive.messages()]) == 2668
 
-    with PffArchive(pst_file) as archive:
+
+def test_pffarchive_load_from_invalid_type():
+
+    with pytest.raises(TypeError):
+        _ = PffArchive(1)
+
+
+def test_pffarchive_iterate_over_folders_dfs(sample_pst_file):
+
+    with PffArchive(sample_pst_file) as archive:
+        for message in archive.messages(bfs=False):
+            assert message.plain_text_body
+
+
+def test_pffarchive_iterate_over_messages(sample_pst_file):
+
+    with PffArchive(sample_pst_file) as archive:
         for message in archive.messages():
             assert message.plain_text_body
