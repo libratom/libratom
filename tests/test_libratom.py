181333f445c0955db16b0586af10f030a85f7c43
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-05-21 18:20:08 -0400

    Add attachment extraction test

--- tests/test_libratom.py
+++ tests/test_libratom.py
@@ -1,6 +1,10 @@
 # pylint: disable=missing-docstring
 import email
+import hashlib
 from email import policy
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 import pytest
 
 import libratom
@@ -14,7 +18,9 @@ def test_version():
 @pytest.mark.parametrize("skip_tree, expected", [(False, True), (True, False)])
 def test_pffarchive_load_from_file_object(sample_pst_file, skip_tree, expected):
 
-    with sample_pst_file.open(mode="rb") as f, PffArchive(f, skip_tree=skip_tree) as archive:
+    with sample_pst_file.open(mode="rb") as f, PffArchive(
+        f, skip_tree=skip_tree
+    ) as archive:
         assert len([message for message in archive.messages()]) == 2668
         assert bool(archive.tree) is expected
 
@@ -41,3 +47,54 @@ def test_pffarchive_format_message(enron_dataset_part004):
                 assert email.message_from_string(
                     archive.format_message(message), policy=policy.default
                 )
+
+
+def test_get_transport_headers_from_sent_items(enron_dataset_part004):
+
+    for pst_file in enron_dataset_part004.glob("*.pst"):
+        with PffArchive(pst_file) as archive:
+            for folder in archive.folders():
+                try:
+                    name = folder.name.lower()
+                except AttributeError:
+                    if folder.identifier != archive.data.root_folder.identifier:
+                        raise
+                    continue
+                if "sent mail" in name or "sent items" in name:
+                    for message in folder.sub_messages:
+                        assert message.transport_headers
+
+
+def test_extract_message_attachments(enron_dataset_part002):
+    """Checking 3 known attachments, to validate the attachment extraction process
+    """
+
+    digests = {
+        47685: "d48232614b01e56014293854abbb5db3",
+        47717: "cf8be7cd3e6e14307972246e2942c9d1",
+        47749: "081e6b66dc89671ff6460adac94dbab1",
+    }
+
+    with PffArchive(
+        next(enron_dataset_part002.glob("*.pst"))
+    ) as archive, TemporaryDirectory() as tmp_dir:
+
+        # Get message by ID
+        node = archive.tree.get_node(2128676)
+        message = node.data
+
+        for att in message.attachments:
+            # Read attachment as bytes
+            rbuf = att.read_buffer(att.size)
+
+            # Save attachment
+            filepath = (
+                Path(tmp_dir) / f"attachment_{message.identifier}_{att.identifier}"
+            )
+            filepath.write_bytes(rbuf)
+
+            # Confirm checksum
+            assert hashlib.md5(rbuf).hexdigest() == digests[att.identifier]
+
+            # Sanity check on the file
+            assert filepath.stat().st_size == att.size
