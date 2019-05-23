e76477f4f32bee0793903cc59b4366a859ea58f1
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-05-23 15:54:21 -0400

    Add logging to load test

--- tests/load/test_pst.py
+++ tests/load/test_pst.py
@@ -1,11 +1,26 @@
 # pylint: disable=missing-docstring
-
+import logging
+import humanfriendly
 from libratom.utils.pff import PffArchive
+
+logger = logging.getLogger(__name__)
 
 
 def test_extract_enron_messages(enron_dataset):
+    nb_extracted = 0
+    total_size = 0
 
     for pst_file in enron_dataset.glob("**/*.pst"):
+
+        # Add file size to running total
+        total_size += pst_file.stat().st_size
+
+        # Iterate over messages and copy message string
         with PffArchive(pst_file) as archive:
             for message in archive.messages():
                 assert archive.format_message(message)
+
+                # Increment message count
+                nb_extracted += 1
+
+    logger.info(f'Extracted {nb_extracted} messages from a total of {humanfriendly.format_size(total_size)}')
