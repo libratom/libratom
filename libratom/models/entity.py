c673a868e6cb762c12e1bc2ac0230ec1d7f6d3bd
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-07-18 20:16:36 -0400

    Entity extraction WIP

--- libratom/models/entity.py
+++ libratom/models/entity.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class Entity(Base):
-    __tablename__ = 'entities'
+    __tablename__ = "entities"
 
     id = Column(Integer, primary_key=True)
     text = Column(String)
@@ -17,4 +17,4 @@ class Entity(Base):
 
     def __str__(self):
         column_names = [col.key for col in self.__table__.columns]
-        return ' '.join([f'{name}: {getattr(self, name)}' for name in column_names])
+        return " ".join([f"{name}: {getattr(self, name)}" for name in column_names])
