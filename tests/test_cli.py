859dfa513d75c5937b2580ed894b9336aa006744
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-07-20 13:03:01 -0400

    Extend CLI unit tests (#24)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,15 +7,36 @@ from libratom.cli.cli import ratom
 
 
 @pytest.mark.parametrize(
-    "options, expected", [
-        (' ', 'Usage'),
-        ('-h', 'Usage'),
-        ('--help', 'Usage'),
-        ('--version', libratom.__version__),
-    ]
+    "params, expected",
+    [
+        ([], "Usage"),
+        (["-h"], "Usage"),
+        (["--help"], "Usage"),
+        (["--version"], libratom.__version__),
+    ],
 )
-def test_ratom(cli_runner, options, expected):
+def test_ratom(cli_runner, params, expected):
 
-    result = cli_runner.invoke(ratom, [options])
+    result = cli_runner.invoke(ratom, args=params)
     assert result.exit_code == 0
     assert expected in result.output
+
+
+@pytest.mark.parametrize(
+    "params, expected",
+    [
+        ([], ["nothing to do"]),
+        (["-h"], ["Usage"]),
+        (["-v"], ["nothing to do", "All done"]),
+    ],
+)
+def test_ratom_entities(isolated_cli_runner, params, expected):
+
+    args = ["entities"]
+    args.extend(params)
+
+    result = isolated_cli_runner.invoke(ratom, args=args)
+    assert result.exit_code == 0
+
+    for token in expected:
+        assert token in result.output
