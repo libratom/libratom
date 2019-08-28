255bb46234638815d5b0bcfe4fc6c00b6d314651
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-08-28 10:59:43 -0600

    Add foreign key relationships between models (#45)

--- libratom/models/message.py
+++ libratom/models/message.py
@@ -1,6 +1,7 @@
 # pylint: disable=too-few-public-methods,missing-docstring,invalid-name
 
 from sqlalchemy import Column, DateTime, ForeignKey, Integer
+from sqlalchemy.orm import relationship
 
 from libratom.lib.database import Base
 from libratom.models.file_report import FileReport
@@ -10,6 +11,8 @@ class Message(Base):
     __tablename__ = "message"
 
     id = Column(Integer, primary_key=True)
+    pff_identifier = Column(Integer)
     processing_start_time = Column(DateTime)
     processing_end_time = Column(DateTime)
-    file_id = ForeignKey(FileReport.id)
+    file_report_id = Column(Integer, ForeignKey(FileReport.id))
+    entities = relationship("Entity", backref="message")
