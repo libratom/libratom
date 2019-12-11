43f44115132727da44a4735aa190898a5a42ed7c
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-12-10 21:11:44 -0500

    Store attachment info in DB (#84)
    closes #84

--- libratom/lib/entities.py
+++ libratom/lib/entities.py
@@ -10,16 +10,17 @@ from collections import namedtuple
 from datetime import datetime
 from importlib import reload
 from pathlib import Path
-from typing import Callable, Dict, Iterable, Optional, Set, Tuple
+from typing import Callable, Dict, Iterable, List, Optional, Set, Tuple
 
 import pkg_resources
 import spacy
 from spacy.language import Language
 from sqlalchemy.orm.session import Session
 
+from libratom.lib.base import AttachmentMetadata
 from libratom.lib.concurrency import get_messages, imap_job, worker_init
 from libratom.lib.core import open_mail_archive
-from libratom.models import Entity, FileReport, Message
+from libratom.models import Attachment, Entity, FileReport, Message
 
 logger = logging.getLogger(__name__)
 
@@ -54,7 +55,11 @@ SPACY_MODELS = namedtuple("SpacyModels", SPACY_MODEL_NAMES)(*SPACY_MODEL_NAMES)
 
 @imap_job
 def process_message(
-    filepath: str, message_id: int, message: str, spacy_model: Language
+    filepath: str,
+    message_id: int,
+    message: str,
+    attachments: List[AttachmentMetadata],
+    spacy_model: Language,
 ) -> Tuple[Dict, Optional[str]]:
     """
     Job function for the worker processes
@@ -65,6 +70,7 @@ def process_message(
         "filepath": filepath,
         "message_id": message_id,
         "processing_start_time": datetime.utcnow(),
+        "attachments": attachments,
     }
 
     try:
@@ -184,6 +190,7 @@ def extract_entities(
                 entities = res.pop("entities")
                 message_id = res.pop("message_id")
                 filepath = res.pop("filepath")
+                attachments = res.pop("attachments")
 
                 # Create new message instance
                 message = Message(pff_identifier=message_id, **res)
@@ -200,6 +207,18 @@ def extract_entities(
                     )
                 message.file_report = file_report
                 session.add(message)
+
+                # Record attachment info
+                session.add_all(
+                    [
+                        Attachment(
+                            **attachment._asdict(),
+                            message=message,
+                            file_report=file_report,
+                        )
+                        for attachment in attachments
+                    ]
+                )
 
                 # Record entities info
                 for entity in entities:
