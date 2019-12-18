6816d3a84a35bd8a3f027f1b38add65262e99f77
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-12-18 18:51:42 -0500

    Update dependencies

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -4,3 +4,8 @@ set -x
 
 pip-compile -o requirements.txt requirements.in --upgrade --quiet "$@"
 pip-compile -o requirements-dev.txt requirements-dev.in requirements.in --upgrade --quiet "$@"
+
+# Python 3.6 dependency only
+# Add to .in files if supported by pip-compile
+echo "importlib_resources; python_version < '3.7'" >> requirements.txt
+echo "importlib_resources; python_version < '3.7'" >> requirements-dev.txt
