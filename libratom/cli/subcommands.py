b6e2be6a15db207be2b06011e9138d4a2a3ab510
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-12-18 09:13:13 -0500

    Pass jobs parameter to scan_files() (#95)
    closes #95

--- libratom/cli/subcommands.py
+++ libratom/cli/subcommands.py
@@ -69,7 +69,7 @@ def entities(
         color="green",
         leave=False,
     ) as file_bar, db_session(Session) as session:
-        status = scan_files(files, session, progress_callback=file_bar.update)
+        status = scan_files(files, session, jobs=jobs, progress_callback=file_bar.update)
 
     if status == 1:
         logger.warning("Aborting")
