f8aed16c2fd4ca8b3a65cc783441c1c784ed15ec
Antoine de Torcy <adetorcy@users.noreply.github.com>
2019-12-19 12:47:59 -0500

    Slightly format requirement files

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -7,5 +7,5 @@ pip-compile -o requirements-dev.txt requirements-dev.in requirements.in --upgrad
 
 # Python 3.6 dependency only
 # Add to .in files if supported by pip-compile
-echo "importlib_resources; python_version < '3.7'" >> requirements.txt
-echo "importlib_resources; python_version < '3.7'" >> requirements-dev.txt
+echo -e "\n\n# PY36\nimportlib_resources; python_version < '3.7'" >> requirements.txt
+echo -e "\n\n# PY36\nimportlib_resources; python_version < '3.7'" >> requirements-dev.txt
