cec681e6d7373403790322e762916915a8d17fe6
Antoine de Torcy <adetorcy@users.noreply.github.com>
2020-03-27 13:27:15 -0400

    Simplify access to non-code data

--- libratom/data/__init__.py
+++ libratom/data/__init__.py
@@ -11,8 +11,13 @@ except ImportError:
     import importlib_resources as resources
 
 
-# Load mime types for direct access
+# Load JSON data for direct access
 with resources.path(__name__, "media_types.json") as media_types_file, open(
     media_types_file, "r"
 ) as fp:
     MIME_TYPES = set(json.load(fp))
+
+with resources.path(__name__, "eml_dump_input.schema.json") as schema_file, open(
+    schema_file
+) as schema_fp:
+    EML_DUMP_INPUT_SCHEMA = json.load(schema_fp)
