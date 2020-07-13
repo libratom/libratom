55cd35a5e8b9ea0a86238d9800f562de95ad3731
Antoine de Torcy <adetorcy@users.noreply.github.com>
2020-07-13 14:54:55 -0400

    Validate message contents in unit test

--- libratom/lib/database.py
+++ libratom/lib/database.py
@@ -7,6 +7,7 @@ from contextlib import contextmanager
 from pathlib import Path
 from typing import ContextManager
 
+from click.testing import Result
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
@@ -37,6 +38,41 @@ def db_session(session_factory: sessionmaker) -> ContextManager[Session]:
     """
 
     session = session_factory()
+
+    try:
+        yield session
+        if session.new or session.dirty or session.deleted:
+            session.commit()
+
+    except SQLAlchemyError as exc:
+        logger.exception(exc)
+        session.rollback()
+
+    finally:
+        session.close()
+
+
+@contextmanager
+def db_session_from_cmd_out(result: Result) -> ContextManager[Session]:
+    """
+    Convenience function to inspect the DB output of a ratom command
+    """
+    # TODO: DRY with db_session above
+    # pylint: disable=no-member
+
+    # Find DB file in command result
+    db_file = None
+    for line in result.output.splitlines():
+        if line.startswith("Creating database file:"):
+            db_file = Path(line.rsplit(maxsplit=1)[1].strip())
+
+    # Sanity check
+    if not (db_file and db_file.is_file()):
+        raise ValueError(f"Invalid database file: {db_file}")
+
+    # Open DB session
+    engine = create_engine(f"sqlite:///{db_file}")
+    session = sessionmaker(bind=engine)()
 
     try:
         yield session
