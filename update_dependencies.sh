15666421a66f778e264be2552526605640453ce1
Antoine de Torcy <adetorcy@users.noreply.github.com>
2021-02-01 17:26:43 -0500

    Drop Python 3.6

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -4,8 +4,3 @@ set -x
 
 pip-compile -o requirements.txt requirements.in --upgrade --quiet "$@"
 pip-compile -o requirements-dev.txt requirements-dev.in requirements.in --upgrade --quiet "$@"
-
-# Python 3.6 dependency only
-# Add to .in files if supported by pip-compile
-echo -e "\n\n# PY36\nimportlib_resources; python_version < '3.7'" >> requirements.txt
-echo -e "\n\n# PY36\nimportlib_resources; python_version < '3.7'" >> requirements-dev.txt
