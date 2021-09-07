ff64a5283731e640aa1adac7bce3c3dea07a25f7
Antoine de Torcy <adetorcy@users.noreply.github.com>
2021-09-07 09:16:52 -0600

    Address linter warnings

--- libratom/data/__init__.py
+++ libratom/data/__init__.py
@@ -7,13 +7,13 @@ from importlib import resources
 
 # Load JSON data for direct access
 with resources.path(__name__, "media_types.json") as media_types_file, open(
-    media_types_file, "r"
+    media_types_file, "r", encoding="utf8", errors="strict"
 ) as fp:
     MIME_TYPES = set(json.load(fp))
 
 MIME_TYPE_REGISTRIES = {mime_type.split("/", maxsplit=1)[0] for mime_type in MIME_TYPES}
 
 with resources.path(__name__, "eml_dump_input.schema.json") as schema_file, open(
-    schema_file
+    schema_file, "r", encoding="utf8", errors="strict"
 ) as schema_fp:
     EML_DUMP_INPUT_SCHEMA = json.load(schema_fp)
