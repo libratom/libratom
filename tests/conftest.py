2734750bea264f8748140374f9587254e825b5d1
Antoine de Torcy <adetorcy@users.noreply.github.com>
2022-02-23 12:55:52 -0500

    Update entity extraction test to use en_core_web_sm 3.2.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -369,12 +369,12 @@ def bad_eml_export_input(eml_export_input_json) -> Path:
 
 
 @pytest.fixture(scope="function")
-def en_core_web_sm_3_1_0() -> None:
-    model, version = "en_core_web_sm", "3.1.0"
+def en_core_web_sm_3_2_0() -> None:
+    model, version = "en_core_web_sm", "3.2.0"
 
     existing_version = get_installed_model_version(model)
 
-    # Install version 3.1.0
+    # Install version 3.2.0
     if existing_version != version:
         assert install_spacy_model(model, version) == 0
 
